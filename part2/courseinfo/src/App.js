e89eb54a346c9b63a1108a409c7c9268db2f7283
bambery <lwszolek@gmail.com>
2023-06-25 14:35:56 -0500

    Completed part2a exercise 2.4: course information step9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,7 +1,7 @@
 const Header = ({ course }) => (
-    <h1>
+    <h2>
         {course}
-    </h1>
+    </h2>
 )
 
 const Part = ({ part }) => (
@@ -34,7 +34,8 @@ const Course = ({ course }) => (
 )
 
 const App = () => {
-    const course = {
+    const courses = [
+    {
         id: 1,
         name: 'Half Stack application development',
         parts: [
@@ -52,11 +53,38 @@ const App = () => {
                 name: 'State of a component',
                 exercises: 14,
                 id: 3
+            },
+            {
+                name: 'Redux',
+                exercises: 11,
+                id: 4
             }
         ]
-    }
+    },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
-    return <Course course={course} />
+    return(
+        <div>
+            <h1>Web development cirriculum</h1>
+            {courses.map(course => <Course key={course.id} course={course} />)}
+        </div>
+    )
 }
 
 export default App;
