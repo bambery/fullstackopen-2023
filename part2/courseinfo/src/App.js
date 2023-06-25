dab7900af7bb53c0f85e709fcad4c2c49a30e7a4
bambery <lwszolek@gmail.com>
2023-06-25 14:39:02 -0500

    Completed part 2a exercise 2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,37 +1,4 @@
-const Header = ({ course }) => (
-    <h2>
-        {course}
-    </h2>
-)
-
-const Part = ({ part }) => (
-    <p>
-        {part.name} {part.exercises}
-    </p>
-)
-
-const Content = ({ parts }) => {
-    return (
-        <div>
-            {parts.map( part => <Part key={part.id} part={part} />)}
-        </div>
-    )
-}
-
-const Total = ({ parts }) => (
-    <p>
-        <b>total of {parts.reduce( (acc, part) => acc + part.exercises, 0)} exercises</b>
-    </p>
-
-)
-
-const Course = ({ course }) => (
-    <div>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-    </div>
-)
+import Course from "./Course"
 
 const App = () => {
     const courses = [
