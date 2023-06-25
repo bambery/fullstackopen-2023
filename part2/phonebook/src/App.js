eefe887764e8f32a727a4ccf151097d6e10f6c44
bambery <lwszolek@gmail.com>
2023-06-25 16:23:05 -0500

    Completed part 2b exercise 2.10: the phonebook step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,35 @@
 import { useState } from 'react'
 
+const PersonList = ({personsToShow}) => (
+    <div>
+        <h2>Numbers</h2>
+        {personsToShow.map(person => <Person key={person.id} person={person}/>)}
+    </div>
+)
+
+const Person = ({person}) => (
+    <div>{person.name} {person.number}</div>
+)
+
+const SearchFilter = ({search, handleSearchChange}) => (
+    <div>
+        filter shown with <input value={search} onChange={handleSearchChange}/>
+    </div>
+)
+
+const PersonForm = (props) => (
+    <form onSubmit={props.addPerson}>
+        <div>
+            <div>name: <input value={props.newName} onChange={props.handleNameChange}/> </div>
+            <div>number: <input value={props.newNumber} onChange={props.handleNumberChange}/> </div>
+        </div>
+        <div>
+            <button type="submit">add</button>
+        </div>
+    </form>
+)
+
+
 const App = () => {
     const [persons, setPersons] = useState([
         { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -43,19 +73,10 @@ const App = () => {
     return (
         <div>
             <h1>Phonebook</h1>
-            <div>filter shown with <input value={search} onChange={handleSearchChange}/> </div>
+            <SearchFilter search={search} handleSearchChange={handleSearchChange}/>
             <h2>Add a New Number</h2>
-            <form onSubmit={addPerson}>
-                <div>
-                    <div>name: <input value={newName} onChange={handleNameChange}/> </div>
-                    <div>number: <input value={newNumber} onChange={handleNumberChange}/> </div>
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            { persons.filter( person => person.name.toUpperCase().includes(search.trim().toUpperCase()) ).map(person => <div key={person.name}>{person.name} {person.number}</div>) }
+            <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
+            <PersonList personsToShow={persons.filter( person => person.name.toUpperCase().includes(search.trim().toUpperCase()) ) } />
         </div>
     )
 }
