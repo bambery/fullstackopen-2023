9048d4691e4a7e507799244e2eb7ffa5744428e0
bambery <lwszolek@gmail.com>
2023-06-28 12:36:34 -0500

    Completed part 3a exercise 3.4: Phonebook backend step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
     {
       "id": 1,
       "name": "Arto Hellas",
@@ -42,6 +42,13 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.get('/info', (request, response) => {
     response.send(`<div><p>Phonebook has info for ${persons.length} people.</p><p>${new Date}</p></div>`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
