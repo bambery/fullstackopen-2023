de766a446686719116d757726a2db072b640f81d
bambery <lwszolek@gmail.com>
2023-07-06 21:55:01 -0500

    Completed part 4d exercise 4.17: bloglist expansion, step5

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,8 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', { username: 1, name: 1 })
+
     response.json(blogs)
 })
 
@@ -17,15 +21,20 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+    const user = await User.findById(body.userId)
 
     const blog = new Blog({
         title: body.title,
         author: body.author,
         likes: body.likes,
-        url: body.url
+        url: body.url,
+        user: user.id
     })
 
     const savedBlog = await blog.save()
+    await savedBlog.populate('user', { username: 1, name: 1 })
+    user.blogs= user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
 })
 
