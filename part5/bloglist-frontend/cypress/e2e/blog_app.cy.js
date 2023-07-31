3643f0ea384bff4a3aca2688cf19a464eae4d9f4
bambery <lwszolek@gmail.com>
2023-07-30 21:20:52 -0500

    Completed part 5d exercise 5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,40 @@
 describe('Blog app', () => {
     beforeEach(() => {
         cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+        const user = {
+            name: 'Susan B. Anthony',
+            username: 'santhony',
+            password: 'secretpassword'
+        }
+        cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
         cy.visit('')
     })
+
     it('Login form is shown', () => {
         cy.contains('Log in to application')
         cy.contains('button', 'login')
+    })
+
+    describe('Login', () => {
+        it('succeeds with correct credentials', () => {
+            cy.contains('button', 'login').click()
+            cy.get('#username').type('santhony')
+            cy.get('#password').type('secretpassword')
+            cy.get('#submit-button').click()
+
+            cy.contains('Susan B. Anthony is logged in')
+        })
+
+        it('fails with wrong credentials', () => {
+            cy.contains('button', 'login').click()
+            cy.get('#username').type('santhony')
+            cy.get('#password').type('incorrectpassword')
+            cy.get('#submit-button').click()
+
+            cy
+                .get('.error')
+                .should('contain', 'Wrong username or password')
+                .and('have.css', 'color', 'rgb(255, 0, 0)')
+        })
     })
 })
