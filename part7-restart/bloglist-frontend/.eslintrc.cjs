be0a7a4278bba749d80cbea85546d8d7728ffbcd
bambery <lwszolek@gmail.com>
2024-01-16 14:33:42 -0600

    getting rid of lint error

--- part7-restart/bloglist-frontend/.eslintrc.cjs
+++ part7-restart/bloglist-frontend/.eslintrc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   root: true,
   env: {
-      browser: true,
-      es2020: true,
-      "jest/globals": true,
-      "cypress/globals": true
+    browser: true,
+    es2020: true,
+    "jest/globals": true,
+    "cypress/globals": true
   },
   extends: [
     'eslint:recommended',
@@ -17,34 +17,36 @@ module.exports = {
     '.eslintrc.cjs'
   ],
   parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module'
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   settings: { react: { version: '18.2' } },
   plugins: [
+    'react',
     'react-refresh',
-      'cypress',
-      'jest'
+    'cypress',
+    'jest'
   ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-      'react/prop-types': 0,
-      "eqeqeq": "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": [
-          "error", "always"
-      ],
-      "template-curly-spacing": [
-          "error", "never"
-      ],
-      "arrow-spacing": [
-          "error", { "before": true, "after": true }
-      ],
-      "no-console": 0,
-      "react/prop-types": 0,
-      "react/react-in-jsx-scope": "off"
+    'react/prop-types': 0,
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "template-curly-spacing": [
+      "error", "never"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": 0,
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "error"
   },
 }
