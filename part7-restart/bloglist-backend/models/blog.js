8c2cc61b02d92e6ce44c3a07e8caa44b84f868d8
bambery <lwszolek@gmail.com>
2024-01-21 14:37:46 -0600

    fixing typo in comments array definition

--- part7-restart/bloglist-backend/models/blog.js
+++ part7-restart/bloglist-backend/models/blog.js
@@ -18,10 +18,12 @@ const blogSchema = new mongoose.Schema({
         type: Number,
         default: 0
     },
-    comments: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Comment'
-    }
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
 })
 
 blogSchema.set('toJSON', {
