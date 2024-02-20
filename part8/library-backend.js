4dee4e9ca2cac40fb63b80a046b82c1b03329d7c
bambery <lwszolek@gmail.com>
2024-02-20 14:36:55 -0600

    completed exercise 8.6: implement addBook, which in addition to creating new books will also create an author if none by that name exist

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -104,6 +105,14 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]
   }
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -126,6 +135,21 @@ const resolvers = {
     bookCount: (root) => books.reduce((count, book) => {
       return book.author === root.name ? count+=1 : count
     }, 0)
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.find(a => a.name === args.author)) {
+        const author = {
+          name: args.author,
+          id: uuid()
+        }
+        authors = authors.concat(author)
+      }
+
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
+    }
   }
 }
 
